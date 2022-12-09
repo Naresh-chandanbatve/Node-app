2bb3a408126a9fe2ee048a724ee9e1a8511d81c1
Naresh <chandanbatven@gmail.com>
2022-12-10 04:28:19 +0530

    test

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+
+const express = require('express');
+
 var msg = "hello world";
  console.log(msg);
+const PORT = 3000;
+const app = express();
+app.get('/',(req,res) => {
+    res.send("this is test success");
+});
+app.listen(PORT, () => console.log(`Server Running at port ${PORT}`));
